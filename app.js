290d58b7cd64945ee7f49bf75c78bcb29e442848
Shyampatidar17 <shyampatidar171995@gmail.com>
2022-01-29 17:29:13 +0530

    html+css-part done for groot= in js alert arise learn

---
+++ app.js
@@ -0,0 +1,7 @@
+console.log("hello script is working")
+var username = prompt("give me username")
+alert("this script works!! " + username)
+var welcomeMessauge="this script work!!  " + username;
+alert(welcomemessage);
+
+// var txtInput = document.querySelector("#txt-input");
