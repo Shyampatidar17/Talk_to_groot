e5dfec959b8604c13e1c817cace00153deec0503
Shyampatidar17 <shyampatidar171995@gmail.com>
2022-01-30 12:43:42 +0530

    going to console and click link translate button highlight

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
-console.log("hello script is working")
-var username = prompt("give me username")
-alert("this script works!! " + username)
-var welcomeMessauge="this script work!!  " + username;
-alert(welcomemessage);
+var btnTranslate= document.querySelector("#btn-translate");
+console.log(btnTranslate);
 
+// btnTranslate.addEventListener("click", function clickEventListener(){
+//     console.log("clicked!")
+// })
 // var txtInput = document.querySelector("#txt-input");
+
+ 
+
+// https://api.funtranslations.com/translate/groot.json
