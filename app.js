98304453f195bd2c0b66d374a54e29f9e8ac8109
Shyampatidar17 <shyampatidar171995@gmail.com>
2022-01-30 19:30:27 +0530

    done output

--- app.js
+++ app.js
@@ -1,6 +1,14 @@
 var btnTranslate= document.querySelector("#btn-translate");
 var txtInput = document.querySelector("#txt-input");
-var outputDiv = document.querySelector("#output")
+var outputDiv = document.querySelector("#output");
+
+// var serverURL ="https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json"
+
+var serverURL ="https://api.funtranslations.com/translate/groot.json"
+
+function getTranslationURL(input){
+    return serverURL + "?" + "text=" + input
+}
 
 // outputDiv.innerText = "shyam patidar"
 
@@ -9,10 +17,16 @@ var outputDiv = document.querySelector("#output")
 function clickHandler() {
     // console.log("clicked!");
     // console.log("input", txtInput.value);
-    outputDiv.innerText = "dshjskj " + txtInput.value
+    // outputDiv.innerText = "dshjskj " + txtInput.value
+    var inputText = txtInput.value;
+    
+    fetch(getTranslationURL(inputText))
+    .then(response => response.json())
+    .then(json => {
+        var translatedText = json.contents.translated;
+        outputDiv.innerText = translatedText;
+        // console.log(json.contents.translated)
+    })
 }
 
 btnTranslate.addEventListener("click", clickHandler);
-
-
-// https://api.funtranslations.com/translate/groot.json
